c67da4f5465585ff482e80d758e15532b1606490
winstonlin0309 <winstonlin0309@gmail.com>
2023-06-28 20:54:11 -0700

    display posts in home page

--- Blog/app.js
+++ Blog/app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -17,7 +18,8 @@ app.use(express.static("public"));
 app.get("/", function(req, res) {
   res.render("home", {
     pageTitle: "Home",
-    startingContent: homeStartingContent
+    startingContent: homeStartingContent,
+    contentPosts : posts
   });
 })
 
@@ -39,7 +41,16 @@ app.get("/compose", function(req, res) {
   res.render("Compose");
 })
 
+app.post("/compose", function(req, res) {
+  let post = {
+    postTitle : req.body.postTitle,
+    postBody : req.body.postBody
+  }
 
+  posts.push(post);
+  res.redirect("/");
+
+})
 
 
 
