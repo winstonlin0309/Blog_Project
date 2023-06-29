9e18a25f9c3c0362c05b933b5cdaf186101f1b9d
winstonlin0309 <winstonlin0309@gmail.com>
2023-06-28 19:28:31 -0700

    include layout to each web page files

--- Blog/app.js
+++ Blog/app.js
@@ -15,7 +15,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home");
+  res.render("home", {
+    title: "Home",
+    content: homeStartingContent
+  });
 })
 
 
